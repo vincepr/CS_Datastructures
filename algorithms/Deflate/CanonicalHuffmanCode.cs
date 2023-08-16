ffb6b55b438858718e979e3d9314ee1985bd6e06
vincepr@rob <vincepr@gmx.de>
2023-08-16 14:56:12 +0200

    work in progress

---
+++ algorithms/Deflate/CanonicalHuffmanCode.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src.algorithms.Deflate
+{
+    internal class CanonicalHuffmanCode
+    {
+        private const int MAX_CODE_LENGTH = 15;
+        private readonly Dictionary<int, int> _bitToSymbol;
+
+        public CanonicalHuffmanCode(in int[] codeLengths)
+        {
+            // check if params are of valid state:
+            foreach( var l in codeLengths)
+            {
+                if (l < 0) throw new ArgumentOutOfRangeException("Negative code length");
+                if (l > MAX_CODE_LENGTH) throw new ArgumentOutOfRangeException("Maximum code length exceeded.");
+            }
+
+            //
+            int nextCode = 0;
+            for (int codeLen=1; codeLen <= MAX_CODE_LENGTH; codeLen++)
+            {
+                nextCode = nextCode << 1;
+                int startBit = 1 << codeLen;
+                for (int symbol = 0; symbol < codeLengths.Length; symbol++)
+                {
+                    if (codeLengths[symbol] != codeLen) continue;
+                    if (nextCode >= startBit) throw new Exception("Canonical code produces illegal OVER-full Huffman-code-tree.");
+                    _bitToSymbol[startBit | nextCode] = symbol;
+                    nextCode++;
+                }
+            }
+            if (nextCode != 1 << MAX_CODE_LENGTH)) throw new Exception("Canonical code produces illegal UNDER-full Huffman-code-tree.");
+        }
+
+
+    }
+}
--- algorithms/Deflate/Decompressor.cs
+++ algorithms/Deflate/Decompressor.cs
@@ -8,14 +8,69 @@ namespace src.algorithms.Deflate
 {
     internal class Decompressor
     {
-        private BitStream input;
-        private Stream output;
-        private ByteHistory history;
+        const int SIZE_OF_HISTORY_IN_BYTES = 32 * 1024;
 
-        
-        public Decompressor()
+        private BitStream _input;
+        private Stream _output;
+        private ByteHistory _history;
+
+        /// <summary>
+        /// starts the decompression process for data in the inputstream and writes it to the output stream.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="output"></param>
+        public static void Decompress(BitStream input, Stream output)
         {
-            
+            new Decompressor(input, output);
+        }
+
+        /// <summary>
+        /// Constructor, that immediatly starts the decompressoin.
+        /// </summary>
+        private Decompressor(BitStream input, Stream output)
+        {
+            _input = input;
+            _output = output;
+            _history = new ByteHistory(SIZE_OF_HISTORY_IN_BYTES);
+
+            // Process of decompression:
+            bool isFinal;
+            {
+                // Header Block
+                isFinal = input.readUint(1) != 0;       // BFINAL
+                uint type = input.readUint(2);          // BTYPE
+
+                // decompress rest of the block depending on type
+                if (type == 0)
+                {
+                    decompressUncompressedBlock();
+                }
+                else if (type == 1)
+                {
+                    decompressHuffmanBlock(FIXED_LENGTH, FIXED_DISTANCE);
+                }
+                else if (type == 2)
+                {
+                    CanonicalCode[] hufLenDist = decodeHuffmanCodes();
+                    decompressHuffmanBlock();
+                }
+                else if (type == 3)
+                {
+                    throw new InvalidDataException("Reserved block type.");
+                }
+                else throw new InvalidDataException("Unreachable");
+            } while (!isFinal) ;
+
+        }
+
+        private void decompressUncompressedBlock()
+        {
+            throw new NotImplementedException();
+        }
+
+        private decompressHuffmanBlock()
+        {
+            throw new NotImplementedException();
         }
     }
 }
